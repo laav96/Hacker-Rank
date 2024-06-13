77db4e0b7c1b37428641319273d34ae5f43dbd17
Akeyla Virgin <93278622+laav96@users.noreply.github.com>
2024-06-12 20:34:45 -0400

    Create Weather Observation Station 1.sql

---
+++ SQL/Basic Select/Weather Observation Station 1.sql
@@ -0,0 +1,14 @@
+-- ========================
+--       Information
+-- ========================
+
+-- Direct Link: https://www.hackerrank.com/challenges/weather-observation-station-1/problem
+-- Max Score: 15
+-- DBMS: mySQL
+
+-- ========================
+--         Solution
+-- ========================
+
+SELECT city, state
+FROM station;
