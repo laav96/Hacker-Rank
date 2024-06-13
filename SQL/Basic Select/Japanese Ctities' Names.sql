97a15441c78bd70e422f60519165946ccc35d3ec
Akeyla Virgin <93278622+laav96@users.noreply.github.com>
2024-06-12 20:36:38 -0400

    Create Japanese Ctities' Names.sql

---
+++ SQL/Basic Select/Japanese Ctities' Names.sql
@@ -0,0 +1,15 @@
+-- ========================
+--       Information
+-- ========================
+
+-- Direct Link: https://www.hackerrank.com/challenges/japanese-cities-name/problem
+-- Max Score: 10
+-- DBMS: mySQL
+
+-- ========================
+--         Solution
+-- ========================
+
+SELECT name
+FROM city
+WHERE CountryCode = 'JPN'
